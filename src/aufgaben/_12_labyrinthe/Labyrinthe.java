9951fb3a61bb3e4753dba5309b8b4dc1bd0fffc2
Liam <codecentricprakti@codecentric.de>
2023-03-27 16:57:38 +0200

    Labyrinthe lösen, Kleeblattreihen sortieren wip

--- src/aufgaben/_12_labyrinthe/Labyrinthe.java
+++ src/aufgaben/_12_labyrinthe/Labyrinthe.java
@@ -4,18 +4,73 @@ import javakara.JavaKaraProgram;
 
 public class Labyrinthe extends JavaKaraProgram {
 
-	public static void main(String[] args) {
-		JavaKaraProgram program = new Labyrinthe();
-		program.run("src/aufgaben/_12_labyrinthe/world1.world");
-		// oder:
-		// program.run("src/aufgaben/_12_labyrinthe/world2.world");
-		// oder:
-		// program.run("src/aufgaben/_12_labyrinthe/world3.world");
-	}
+    public static void main(String[] args) {
+        JavaKaraProgram program = new Labyrinthe();
+        JavaKaraProgram.MIN_SLEEP_TIME = 75;
+        // program.run("src/aufgaben/_12_labyrinthe/world1.world");
+        // oder:
+        // program.run("src/aufgaben/_12_labyrinthe/world2.world");
+        // oder:
+        // program.run("src/aufgaben/_12_labyrinthe/world3.world");
+        program.run("src/aufgaben/_12_labyrinthe/world4.world");
+    }
 
-	@Override
-	public void myMainProgram() {
-		// hier kommt das Hauptprogramm Ihrer L�sung hin
-	}
+    @Override
+    public void myMainProgram() {
+        while (!kara.onLeaf()) {
+            anReihenAnfangStellen();
+            nachRechtsDrehen();
+            nebenAusgangStellen();
+            moveIntoNextRow();
 
-}
+        }
+        kara.removeLeaf();
+    }
+
+    private void nachRechtsDrehen() {
+        turn180();
+    }
+
+    private void anReihenAnfangStellen() {
+        if (kara.treeLeft() || kara.treeRight()) {
+            moveForwardUntilTree();
+        } else {
+            kara.turnRight();
+            moveForwardUntilTree();
+        }
+    }
+
+    private void moveForwardUntilTree() {
+        while (!kara.treeFront()) {
+            kara.move();
+        }
+    }
+
+    void turn180() {
+        kara.turnLeft();
+        kara.turnLeft();
+    }
+
+    void moveIntoNextRow() {
+
+        kara.setDirection(0);
+        if (!kara.treeFront()) {
+            kara.move();
+            kara.move();
+            kara.turnRight();
+            if (!kara.onLeaf()) {
+                kara.move();
+            }
+        }
+            kara.turnLeft();
+
+    }
+        void nebenAusgangStellen () {
+            while (kara.treeLeft() && kara.treeRight()) {
+                kara.move();
+            }
+        }
+
+    }
+
+
--- src/aufgaben/_15_bubblesort/BubbleSort.java
+++ src/aufgaben/_15_bubblesort/BubbleSort.java
@@ -2,6 +2,8 @@ package aufgaben._15_bubblesort;
 
 import javakara.JavaKaraProgram;
 
+import java.util.Arrays;
+
 public class BubbleSort extends JavaKaraProgram {
 
 	public static void main(String[] args) {
@@ -16,6 +18,63 @@ public class BubbleSort extends JavaKaraProgram {
 	@Override
 	public void myMainProgram() {
 		// hier kommt das Hauptprogramm Ihrer L�sung hin
-	}
+		int numberofleaf = 0;
+		int noleaf = 0;
+		int x = 0;
+		int y = 0;
+		int stopp = 0;
+		int[] A = new int[world.getSizeY()];
+		while (y < world.getSizeY()) {
+			noleaf = 0;
+			while (noleaf == 0 && x + 1 < world.getSizeX()) {
+				noleaf++;
+				if (world.isLeaf(x, y)) {
+					numberofleaf++;
+					x++;
+					noleaf = 0;
+				}
 
+			}
+			A[y] = numberofleaf;
+			y++;
+		}
+		System.out.println(Arrays.toString(A));
+		y = 0;
+		int changedSomething = 1;
+		while (changedSomething == 1) {
+			changedSomething = 0;
+
+			while (y < world.getSizeY())
+				if (A[y] > A[y + 1]) {
+					changedSomething = 1;
+
+					while (stopp == 0) {
+
+						if (!world.isLeaf(A[y], y)) {
+							world.setLeaf(A[y], y, true);
+
+						} else {
+							stopp = 1;
+						}
+
+					}
+					x = y + 1;
+					stopp = 0;
+					while (stopp == 0) {
+
+						if (!world.isLeaf(A[x], y)) {
+							world.setLeaf(A[x], y, true);//removeLeaf
+
+						} else {
+							stopp = 1;
+						}
+					}
+				} else {
+					y++;
+				}
+		}
+
+
+	}
 }
+
