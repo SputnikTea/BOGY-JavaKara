9e9701a584b5ba470e96c4f4d76eb0252beaadae
Liam <codecentricprakti@codecentric.de>
2023-03-29 16:27:09 +0200

    Game of Life start wip

--- src/aufgaben/_13_gameoflife/GameOfLife.java
+++ src/aufgaben/_13_gameoflife/GameOfLife.java
@@ -2,6 +2,8 @@ package aufgaben._13_gameoflife;
 
 import javakara.JavaKaraProgram;
 
+import java.util.Arrays;
+
 public class GameOfLife extends JavaKaraProgram {
 
 	public static void main(String[] args) {
@@ -15,7 +17,48 @@ public class GameOfLife extends JavaKaraProgram {
 
 	@Override
 	public void myMainProgram() {
-		// hier kommt das Hauptprogramm Ihrer L�sung hin
+
+		int[] vonKleeblaetternBedeckt = new int[world.getSizeY() * world.getSizeX()];
+		int[] AnzahlNachbarfelder = new int[world.getSizeY() * world.getSizeX()];
+
+		ArrayWoKleeblaetterSind(vonKleeblaetternBedeckt);
+
+		int AnzahlNachbarfelderV=0;
+		int Element=0;
+		while(Element< world.getSizeX()* world.getSizeY()){
+			if(vonKleeblaetternBedeckt[Element]==1){
+				AnzahlNachbarfelderV=0;
+				if(vonKleeblaetternBedeckt[Element]< (world.getSizeY()* world.getSizeX())-1 && vonKleeblaetternBedeckt[Element+1]==1){
+					AnzahlNachbarfelderV++;
+				}
+				if(vonKleeblaetternBedeckt[Element]>0 && vonKleeblaetternBedeckt[Element-1]==1){
+					AnzahlNachbarfelderV++;
+				}
+			}
+		}
 	}
 
+	private void ArrayWoKleeblaetterSind(int[] vonKleeblaetternBedeckt) {
+		int y = 0;
+		int x=0;
+		int yWert=0;
+
+		while (y < world.getSizeY()) {
+
+			while (x < world.getSizeX()) {
+				System.out.println(x);
+			if(world.isLeaf(x,y)){
+				vonKleeblaetternBedeckt[x+yWert]=1;
+			}
+			else{
+				vonKleeblaetternBedeckt[x+yWert]=0;
+			}
+			x++;
+			}
+			y++;
+			x=0;
+			yWert=y* world.getSizeX();
+			System.out.println(Arrays.toString(vonKleeblaetternBedeckt));
+		}
+	}
 }
